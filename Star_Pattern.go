c5660e8a14dc06a53ed6e8d8ff093cfa13a56025
tavadedipak <60614007+tavadedipak@users.noreply.github.com>
2020-04-16 14:49:43 +0530

    /GolangTraining/Day1

---
+++ Star_Pattern.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var rows int
+	fmt.Print("Enter number of rows :")
+    fmt.Scan(&rows)   
+	for i := 1; i <= rows; i++ {
+		for j := 1; j <= i; j++ {
+			fmt.Printf("*")
+		}
+		fmt.Println()
+	}
+}
